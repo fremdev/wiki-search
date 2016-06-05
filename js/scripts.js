ec80ea3aa58b6f8def104a369d9c9eb74ab1ab08
Alex Efremov <alex.efremov.dev@gmail.com>
2016-06-05 13:53:27 +0300

    Add case when empty object return - nothing found message

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,21 @@ $(document).ready(function() {
   function getArticles(userQuery) {
     var wikiApiUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + userQuery + '&callback=?';
     $.getJSON(wikiApiUrl, function(articles) {
-        renderArticlesMarkup(articles.query.pages);
+        renderArticlesMarkup(articles);
       });
     }
 
   function renderArticlesMarkup(articles) {
     var articlesMarkup = '';
-    for (var property in articles) {
-      if (articles.hasOwnProperty(property)) {
-        articlesMarkup += '<div class="article"><h2>' + articles[property].title + '</h2><div class="snippet">' + articles[property].extract + '</div></div>';
+    if (articles.query === undefined) {
+      articlesMarkup += '<div class="error">Nothing found. Try another query</div>';
+    }
+    else {
+      var pages = articles.query.pages;
+      for (var property in pages) {
+        if (pages.hasOwnProperty(property)) {
+          articlesMarkup += '<div class="article"><h2>' + pages[property].title + '</h2><div class="snippet">' + pages[property].extract + '</div></div>';
+        }
       }
     }
     $('.result').html(articlesMarkup);
